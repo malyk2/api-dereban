74a362dab29a7d66c5ae963d96b1bdfafa3e95ca
Denys Domanskyi <manager@div-art.com>
2017-12-15 10:45:13 +0200

    Remove test mail template in routes

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,3 @@
 Route::get('/', function () {
     echo "<h1>API</h1>";
 });
-
-Route::get('/mailable', function () {
-    $user = App\User::find(1);
-
-    return new App\Mail\UserCreate($user);
-});
