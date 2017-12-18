5075a2116a614be270535610450d34d18cf54f76
Taras Kulyk <tk@div-art.com>
2017-12-18 13:02:17 +0200

    add method for groups (add&list)

---
+++ app/Group.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    const STATUS_NEW = 0;
+    const STATUS_ACTIVE = 1;
+
+    protected $fillable = [
+        'name', 'status'
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany('App\User', 'user_group');
+    }
+}
--- resources/lang/uk/validation.php
+++ resources/lang/uk/validation.php
@@ -1,27 +1,30 @@
 <?php
 return [
     'url_has_hash' => ':attribute не містить секцію {hash} для подальшої заміни.',
+    'required' => "Поле :attribute обов'язкове.",
+    'min'                  => [
+        'numeric' => 'Поле :attribute має бути мінімум :min.',
+        'file'    => 'Поле :attribute має бути мінімум :min кілобайт.',
+        'string'  => 'Поле :attribute має бути мінімум :min символів.',
+        'array'   => 'Поле :attribute має містити мінімум :min елементів.',
+    ],
     'custom' => [
         "email" => [
-            "required" => "Поле email обов'язкове.",
             "email" => "Не валідний email.",
             "unique" => "Email вже використовується."
         ],
         "password" => [
-            "required" => "Поле password обов'язкове.",
             "min" => ":Attribute мае бути мінімум :min символів."
         ],
         "url" => [
-            "required" => "Поле url обов'язкове.",
             "url" => "Не валідний url.",
+        ],
+        "lang" => [
+            "size" => "lang має містити :size символи.",
+            "in" => "Некоректне значення lang."
         ],
         "hash" => [
             "required" => "Поле hash обов'язкове.",
         ],
-        "lang" => [
-            "required" => "Поле lang обов'язкове.",
-            "size" => "lang має містити :size символи.",
-            "in" => "Некоректне значення lang."
-        ]
     ],
 ];
