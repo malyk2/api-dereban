7e721367c36cafbb901a4005fcf672490e85dec5
Taras Kulyk <taras.kulyk1@gmail.com>
2018-03-06 14:04:43 +0200

    add swwager 2.0

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,15 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-
+/**
+ * @SWG\Swagger(
+ *   basePath="/",
+ *   @SWG\Info(
+ *     title="Dereban API",
+ *     version="0.0.1"
+ *   )
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
